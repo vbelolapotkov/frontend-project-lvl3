0f755e5ddab3262b6aeb048136d76b795c4cc125
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-11-08 17:32:02 +0300

    Add webpack and bootstrap

--- Makefile
+++ Makefile
@@ -3,6 +3,13 @@ install: install-deps
 install-deps:
 	npm ci
 
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
+
+dev:
+	npx webpack serve
+
 lint:
 	npx eslint .
 
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 8000,
+  },
+};
