f084473b79ef9c5da4f929194aac50796a24ab30
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-12-06 13:27:53 +0300

    Add bootstrap styles

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,28 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader', // inject CSS to page
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          },
+          {
+            loader: 'postcss-loader', // Run postcss actions
+            options: {
+              postcssOptions: {
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
     ],
   },
   plugins: [
